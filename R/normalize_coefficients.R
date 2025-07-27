2f8f1021a5c0e3d5cf3cc0d2e183d4115f305b05
westford14 <westford14@gmail.com>
2025-07-27 13:06:10 +0200

    Adding the code to generate an update rho (validity coefficient)

---
+++ R/normalize_coefficients.R
@@ -0,0 +1,40 @@
+#' Normalize the upper and lower bounds of a validity coefficient
+#'
+#' Using the Fisher Z Transformation, normalize the upper and lower
+#' boundary of a validity coefficient to generate a Normal distribution
+#' based on the mean and standard deviation. The default behavior is
+#' to assume a 95% CI, but this can also be specified via the `interval`
+#' parameter if 99% or 90% CI is preferred.
+#'
+#' @param n the samples for the normal distribution
+#' @param lower_bound numeric the lower boundary of the validity coefficient
+#' @param upper_bound numeric the upper boudnary of the validity coefficient
+#' @param interval numeric the confidence interval to use (default 0.95)
+#' @return Normal distribution
+#' @export
+#'
+#' @examples
+#' n <- 1000
+#' coef_upper <- 0.9
+#' coef_lower <- 0.3
+#' ci <- 0.95
+#' normalize_coefficients(n, coef_lower, coef_upper, ci)
+normalize_coefficients <- function(
+  n,
+  lower_bound,
+  upper_bound,
+  interval = 0.95
+) {
+  stopifnot(lower_bound < upper_bound)
+  stopifnot(interval > 0 && interval < 1)
+  stopifnot(lower_bound > 0 && lower_bound < 1)
+  stopifnot(upper_bound > 0 && upper_bound < 1)
+  stopifnot(n > 0)
+
+  cv <- qnorm((1 - interval) / 2, lower.tail = FALSE)
+  z_lower <- fisher_z_transform(lower_bound)
+  z_upper <- fisher_z_transform(upper_bound)
+  mu <- 0.5 * (z_lower + z_upper)
+  sd <- (0.5 * (z_upper - z_lower)) / cv
+  rnorm(n, mu, sd)
+}
---
+++ tests/testthat/test-generate_coefficient.R
@@ -0,0 +1,48 @@
+test_that("error for CI greater than 1", {
+  expect_error(generate_coefficient()(10, 0.3, 0.7, 10))
+})
+
+test_that("error for CI less than 0", {
+  expect_error(generate_coefficient(10, 0.3, 0.7, -1))
+})
+
+test_that("error for upper bound less than 0", {
+  expect_error(generate_coefficient(10, 0.3, -1, 0.95))
+})
+
+test_that("error for upper bound greater than 1", {
+  expect_error(generate_coefficient(10, 0.3, 1.1, 0.95))
+})
+
+test_that("error for lower bound less than 0", {
+  expect_error(generate_coefficient(10, -1, 0.3, 0.95))
+})
+
+test_that("error for lower bound greater than 1", {
+  expect_error(generate_coefficient(10, 1.1, 0.3, 0.95))
+})
+
+test_that("error for lower bound less than 0", {
+  expect_error(generate_coefficient(10, -1, 0.3, 0.95))
+})
+
+test_that("error samples less than 0", {
+  expect_error(generate_coefficient(-1, 0.3, 0.7, 0.95))
+})
+
+test_that("error lower bound greater than upper bound", {
+  expect_error(generate_coefficient(100, 0.7, 0.3, 0.95))
+})
+
+test_that("test that generating the coefficient works`1", {
+  set.seed(1234)
+  reps <- c()
+  for (i in 1:1000) {
+    reps <- append(reps, generate_coefficient(10000, 0.3, 0.8, 0.95))
+  }
+  expect_equal(
+    mean(reps),
+    0.607,
+    tolerance = 1e-3
+  )
+})
