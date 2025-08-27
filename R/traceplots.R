c951b565bff52627553b8622ed176f2ddcd1118c
westford14 <westford14@gmail.com>
2025-08-27 13:44:08 +0200

    Updating and refactoring the plottting code

--- R/traceplots.R
+++ R/traceplots.R
@@ -10,61 +10,50 @@
 #' @param stan bool If the model is stan based
 #' @return plotted objects
 #' @export
-#' @importFrom rstan traceplot
-#' @importFrom coda traceplot
 #' @import tools
 #' @import ggplot2
-#' @import graphics
+#' @import bayesplot
 traceplots <- function(input, columns, pre_model = FALSE, stan = FALSE) {
-  if (length(columns) == 1) {
-    graphics::par(mfrow = c(1, 1))
-  } else if (length(columns) <= 4) {
-    graphics::par(mfrow = c(2, 2))
-  } else {
-    graphics::par(mfrow = c(ceiling(length(columns) / 2), 2))
-  }
   if (pre_model) {
     if (stan) {
       model_columns <- names(input)
-      target_columns <- model_columns[grepl("sigma", model_columns)]
-
-      plots <- list()
-      for (i in seq_along(target_columns)) {
-        plot <- rstan::traceplot(
-          input,
-          pars = c(target_columns[[i]])
-        ) + ggplot2::labs(
-          title = paste0(
-            "SD for observed ",
-            tools::toTitleCase(columns[i])
-          )
-        )
-        plots[[columns[i]]] <- plot
-      }
-
-      rows <- length(columns)
-      cols <- 1
-      return(plot_a_list(plots, rows, cols))
+      addition <- "Stan"
     } else {
       model_columns <- colnames(input[[1]])
-      target_columns <- model_columns[grepl("sigma", model_columns)]
+      addition <- "JAGS"
+    }
+    target_columns <- model_columns[grepl("sigma", model_columns)]
 
-      for (i in seq_along(target_columns)) {
-        print(coda::traceplot(
-          input[[1]][, target_columns[i]],
-          main = paste0(
-            "SD for observed ",
-            tools::toTitleCase(columns[i])
-          )
-        ))
-      }
+    plots <- list()
+    for (i in seq_along(target_columns)) {
+      plot <- bayesplot::mcmc_trace(
+        input,
+        pars = target_columns[[i]]
+      ) + ggplot2::labs(
+        title = paste0(
+          "SD for Observed ",
+          tools::toTitleCase(columns[[i]]),
+          " (",
+          addition,
+          ")"
+        )
+      )
+      plots[[target_columns[i]]] <- plot
     }
+    rows <- length(target_columns)
+    cols <- 1
+    return(plot_a_list(plots, rows, cols))
   } else {
+    plots <- list()
     for (col in columns) {
-      print(coda::traceplot(
-        input[, col],
-        main = toTitleCase(col)
-      ))
+      plot <- bayesplot::mcmc_trace(
+        input,
+        pars = col
+      ) + ggplot2::labs(title = paste0("Traceplot ", tools::toTitleCase(col)))
+      plots[[col]] <- plot
     }
+    rows <- length(columns)
+    cols <- 1
+    return(plot_a_list(plots, rows, cols))
   }
 }
